08badae64edc2458afc2ed1877f759710c9a8278
Jeremy Andrews <thejeremy.net@gmail.com>
2023-09-25 21:37:27 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module JmaJeremy/dirsizecompare
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func getDirSize(dir string) int64 {
+	var totalSize int64 = 0
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			totalSize += info.Size()
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	return totalSize
+}
+
+func main() {
+	var orig_sz int64
+	var dest_sz int64
+	var pct float64
+
+	orig_sz = getDirSize(os.Args[1])
+	dest_sz = getDirSize(os.Args[2])
+	fmt.Println("Original size: ", orig_sz)
+	fmt.Println("Destination size: ", dest_sz)
+	pct = (float64(dest_sz) / float64(orig_sz)) * 100
+	fmt.Printf("%.2f%% \n", pct)
+}
